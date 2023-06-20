25279c3e100f230412b11687c7d90fae6ae3aef6
Di Fei Chen <chen.daniel2099@gmail.com>
2023-06-20 19:54:01 -0400

    Basic Backend and UI setup

---
+++ project-collab-rest/src/test/java/net/tvslc/projectcollab/ProjectCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tvslc.projectcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
