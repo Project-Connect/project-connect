e7ea25bb0660ad20dc425258c73175502b182412
Wanqing18 <wzhan737@uwo.ca>
2023-06-26 23:39:17 -0400

    addiing one testing case for coverRequest to Entity

--- project-connect-rest/src/test/java/net/tvslc/projectconnect/mapper/RegistrationRequestMapperTest.java
+++ project-connect-rest/src/test/java/net/tvslc/projectconnect/mapper/RegistrationRequestMapperTest.java
@@ -1,21 +1,30 @@
 package net.tvslc.projectconnect.mapper;
 
-import org.junit.jupiter.api.Test;
+import net.tvslc.projectconnect.model.RegistrationRequest;
+import net.tvslc.projectconnect.model.UserEntity;
+import org.junit.jupiter.api.Test;// junit: java build in testing library
 import static org.junit.Assert.assertEquals;
 
 public class RegistrationRequestMapperTest {
 
 
-    @Test
-    public void shouldCovertValidRequestToEntity(){
-        RegistrationRequestMapper mapper = new RegistrationRequestMapper();
+    @Test// the below function is for testing different function for a specific class<unit test>
+    public void shouldCovertValidRequestToEntity(){// create function to test 1 activity, for automation
+        RegistrationRequestMapper mapper = new RegistrationRequestMapper();// create an instance for mapper_function
 
-        // Create a sample request
+        // Create a sample request, <username, password,email,bio>
+        RegistrationRequest testRequest = new RegistrationRequest("Mom321@//","123abc@","hello$@gmail.com","hello, Mom!");
 
         // Pass sample request into mapper.convertToEntity function and store result in a variable
+        UserEntity testEntity = mapper.convertToEntity(testRequest);
 
         // Validate Result using assert
-        assertEquals(1,1);
+        // first test content and type
+        assertEquals(null,testEntity.getId()); // in convert function no id get insert, should be null
+        assertEquals("Mom321@//",testEntity.getUsername());
+        assertEquals("123abc@",testEntity.getPassword());
+        assertEquals("hello$@gmail.com",testEntity.getEmail());
+        assertEquals("hello, Mom!",testEntity.getBio());
     }
 
 }
